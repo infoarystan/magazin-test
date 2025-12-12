6774211a79a331935d3b6653be529afbade47d87
infoarystan <info.arystan@gmail.com>
2025-12-12 08:54:42 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const tg = window.Telegram.WebApp;
 tg.expand();
 
 // Твоя ссылка на сервер
-const API_URL = "https://donerkingkz.hooks.n8n.cloud/"; 
+const API_URL = "https://afraid-ape-11.hooks.n8n.cloud/webhook/order"; 
 
 const products = [
     { id: 1, name: "Шаурма", price: 1800, img: "https://infoarystan.github.io/magazin-test/shaurma.jpg" },
